6826b86712a21cb61ff17e27bce74978aa632f5d
Sofiia Morozova <morozova.s@phystech.edu>
2025-05-23 03:10:02 +0300

    acos

---
+++ include/config.h
@@ -0,0 +1,8 @@
+/* File: include/config.h */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MAX_UIDS 16
+
+#endif
---
+++ test/test_exec.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "Устанавливаем модуль..."
+sudo insmod src/uid_exec.ko
+
+echo "Разрешим юзеру 666 для наглядности"
+echo "666" | sudo tee /proc/exec_allow
+
+echo "Запускаем ls - должно сработать для юзера 666:"
+ls
+
+echo "Запускаем ls для другого юзера - должно заблочить:"
+sudo -u nobody ls || echo "Заблокирован(((."
+
+echo "Удаляем модуль..."
+sudo rmmod uid_exec
